fe73f4dca3f39862ab79d5e544d4080852d0f1bc
Marcelo Cardozo <marcelo.r.cardozo.g@gmail.com>
2020-10-17 17:50:00 -0300

    Return qr as png image

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,14 +1,15 @@
 import {Router} from "express";
 import * as WhatsappService from "../services/whatsapp";
+import {image as qrImage} from "qr-image";
 
 const router = Router();
 
-type QrResponse = {qr: string};
 
 router.get("/qr", async (req, res, next) => {
     const qr = await WhatsappService.getQr();
-    const qrResponse : QrResponse = {qr};
-    res.status(200).json(qrResponse)
+    let code = qrImage(qr, { type: 'png' });
+    res.setHeader('Content-type', 'image/png');  //sent qr image to client side
+    code.pipe(res);
 })
 
 export default router;
